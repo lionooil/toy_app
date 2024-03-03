e3865dc8519649152fef978869c46b8e608d3d52
lionooil <lionooil888@gmail.com>
2024-03-03 01:57:54 +0000

    micropost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_084228) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_03_015441) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
